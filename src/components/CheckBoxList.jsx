9faa922625ee34e7714f77a99722bb5038b54ab8
qwrwed <razk@hotmail.co.uk>
2021-11-21 21:20:54 +0000

    add dynamic checkbox list

---
+++ src/components/CheckBoxList.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable */
+import React from "react";
+
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+const CheckBoxList = ({ listState, setListState, listLabels = {} }) => (
+  // listState is an object where each property name is a key and each property value is a bool
+  // e.g. {a: true, b: false}
+  // listState and setListState should also be the results of useState i.e.
+  //  const [listState, setListState] = useState(defaultListState)
+  <FormGroup>
+    {Object.entries(listState).map(([key, value]) => (
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={listState[key]}
+            onChange={(e) => {
+              const updatedKey = e.target.name;
+              const updatedValue = e.target.checked;
+              setListState({
+                ...listState,
+                ...{ [updatedKey]: updatedValue },
+              });
+            }}
+          />
+        }
+        label={listLabels[key] ?? key}
+        key={key}
+        name={key}
+      />
+    ))}
+  </FormGroup>
+);
+export default CheckBoxList;
--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,18 +1,59 @@
 /* eslint-disable */
-import React from "react";
+import React, { useState } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 // import Box from "@mui/material/Box";
 import { Button, Paper } from "@mui/material";
 
+
+import CheckBoxList from "./CheckBoxList";
 import logo from "../logo.svg";
 import "./App/App.css";
 
+
+
+const sampleListLabels = {
+  "NaptanMetroStation": "Underground/Overground",
+  "NaptanRailStation": "National Rail",
+  "NaptanBusCoachStation": "Bus Stations",
+  "NaptanPublicBusCoachTram": "Bus/Tram Stops",
+  "NaptanFerryPort": "River Transport",
+};
+// https://www.freecodecamp.org/news/how-to-work-with-multiple-checkboxes-in-react/
+
+const objectToList = (obj) => {
+  // remove pairs where value is false, then extract only keys to list
+  return Object.entries(obj)
+    .filter(([key, value]) => value)
+    .map(([key, value]) => key)
+    .join();
+};
+
+
+
 const App = () => {
+
+  const [sampleListState, setSampleListState] = useState({
+    NaptanMetroStation: true,
+    NaptanRailStation: false,
+    NaptanBusCoachStation: true,
+    NaptanPublicBusCoachTram: true,
+    NaptanFerryPort: false,
+  });
+
   return (
     <Container maxWidth="sm" className="App">
       <Paper>
         <img src={logo} className="App-logo" alt="logo" />
+        <Container>===</Container>
+        <CheckBoxList
+          listState={sampleListState}
+          setListState={setSampleListState}
+          listLabels={sampleListLabels}
+        />
+        <Container>{objectToList(sampleListState)}</Container>
+        <Container>===</Container>
+
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App + Material-UI
         </Typography>
