56764070aa4ee609918b3dea9fd7954bbd544506
qwrwed <qwrwed@users.noreply.github.com>
2022-01-30 00:41:00 +0000

    run eslint

--- src/index.jsx
+++ src/index.jsx
@@ -17,7 +17,7 @@ ReactDOM.render(
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
