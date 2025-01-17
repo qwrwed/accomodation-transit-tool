b53b0504608027588df94c3a1936cc0ac2b127f1
qwrwed <2429893+qwrwed@users.noreply.github.com>
2025-01-17 04:53:21 +0000

    fix eslint

--- src/components/Map/ArrowheadsPolyline.jsx
+++ src/components/Map/ArrowheadsPolyline.jsx
@@ -9,6 +9,7 @@ const ArrowheadsPolyline = ({ arrowheads, ...props }) => {
     const polyline = polylineRef.current;
     if (arrowheads && polyline) {
       polyline.arrowheads(arrowheads);
+      // eslint-disable-next-line no-underscore-dangle
       polyline._update();
     }
 
@@ -20,6 +21,7 @@ const ArrowheadsPolyline = ({ arrowheads, ...props }) => {
     };
   }, [arrowheads]);
 
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Polyline ref={polylineRef} {...props} />;
 };
 
